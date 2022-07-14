99b79b1d3f540b00ee240d8bd6304999a6f30007
MA5TERMIND2020 <77905424+MA5TERMIND2020@users.noreply.github.com>
2022-07-14 18:04:31 -0400

    Revert "Minor Syntax Changes"
    This reverts commit 40159a03d4993d68ce68ec3c6d441f95a96d75c1.

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,10 @@
 require('dotenv').config()
-import express from 'express'
+const express = require('express')
 const app = express()
-import { connect, connection } from 'mongoose'
+const mongoose = require('mongoose')
 
-connect(process.env.DATABASE_URL)
-const db = connection
+mongoose.connect(process.env.DATABASE_URL)
+const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
 
